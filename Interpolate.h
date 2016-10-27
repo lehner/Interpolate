90ccc886f6839414f74eaa282ce4276b457202b2
Christoph Lehner <christophlehner@Christophs-iMac.local>
2016-10-27 15:35:33 -0400

    move predefined interpolators in header file

--- Interpolate.h
+++ Interpolate.h
@@ -125,3 +125,73 @@ template<class C,class I> class Interpolate {
   }
 
 };
+
+
+
+
+template<class C> struct simple_int {
+  
+  typedef Interpolate<C,simple_int<C> > IP;
+  C operator()(IP* ip, std::vector<int>& il, std::vector<double>& vl) {
+    
+    // do interpolation
+    C v0 = ip->get(ip->get_index(il));
+    size_t  j;
+    for (j=0;j<ip->dims.size();j++) {
+      C dj = ip->get(ip->get_index(ip->index_plus(il,j))) - v0;
+	
+      v0 += dj * vl[j];
+    }
+      
+    return v0;
+  }
+    
+};
+
+template<class C> struct nlinear_int {
+  
+#define BIT(b) ( (1<<b) )
+
+  typedef Interpolate<C,nlinear_int<C> > IP;
+  C operator()(IP* ip, std::vector<int>& il, std::vector<double>& vl) {
+
+    //
+    // n-dimensional box, interpolate one dimension at a time
+    //
+    // Q_{000...0} is fundamental vertex of box
+    // Q_{100...0} is 1 hop in 0-dimension
+    // ....
+    // Q_{111...1} is 1 hop in all dimensions
+    //
+    // for n dimensions this means 2^n points are involved in interpolation
+    // for n = 5 this means 32 points
+    //
+
+    std::vector<C> Q(1<<il.size());
+    size_t points, d;
+    for (points=0;points<Q.size();points++) {
+	std::vector<int> i = il;
+	for (d=0;d<il.size();d++)
+	  if (BIT(d) & points)
+	    i = ip->index_plus(i,d);
+
+	//printf("Point %d: %d %d %d %d %d\n",points,i[0],i[1],i[2],i[3],i[4]);
+        Q[points] = ip->get(ip->get_index(i));
+    }
+
+    // exit(1);
+    // do interpolation for each dimension, each time reducing Q.size() by a factor of two
+    for (d=0;d<il.size();d++) {
+	std::vector<C> Qp;
+	double a = 1.0 - vl[d];
+	double b = vl[d];
+	// all even indices in Q are at x0, all odd indices are at x1 ->
+	for (points=0;points<Q.size();points+=2)
+	  Qp.push_back(Q[points]*a + Q[points+1]*b);
+	Q = Qp;
+    }
+
+    return Q[0];
+  }
+    
+};
